a230ed90887b00f04d8e0989852171c86e0e9e48
Your Name <you@example.com>
2023-07-24 13:27:59 +0100

    implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,12 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
-
+import searchView from './views/searchView.js';
 import 'core-js/stable'; // For polyfilling everything else
 import 'regenerator-runtime/runtime'; // For polyfilling sync-await
-
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -28,8 +21,25 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Render search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
