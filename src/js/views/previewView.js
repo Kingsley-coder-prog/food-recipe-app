63fc9974ce96ba8adbc74a6270da7a196470569b
Your Name <you@example.com>
2023-07-26 15:56:17 +0100

    uploading new recipe

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -8,7 +8,7 @@ class PreviewView extends View {
     const id = window.location.hash.slice(1);
 
     return `
-    <li class="preview">
+    <li class="preview"> 
             <a class="preview__link ${
               this._data.id === id ? 'preview__link--active' : ''
             } " href="#${this._data.id}">
@@ -18,6 +18,13 @@ class PreviewView extends View {
               <div class="preview__data">
                 <h4 class="preview__title">${this._data.title}</h4>
                 <p class="preview__publisher">${this._data.publisher}</p>
+                <div class="preview__user-generated ${
+                  this._data.key ? '' : 'hidden'
+                }">
+                  <svg>
+                  <use href="${icons}#icon-user"></use>
+                  </svg>
+                </div>
               </div>
             </a>
           </li>
