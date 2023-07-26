ee07af604c4f67f7313db58d3c25cd3f6d029ab2
Your Name <you@example.com>
2023-07-26 15:10:05 +0100

    uploading a new recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '153f1845-562c-4eb6-8dc6-35dcfec6f00c';
+export const MODAL_CLOSE_SEC = 2.5;
